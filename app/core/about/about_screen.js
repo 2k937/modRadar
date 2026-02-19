9f2ceba8f19a1bc0eaf59e84f0970f2018f2e9a5
ACE <jokeacearooni@gmail.com>
2026-02-19 16:54:18 -0600

    Update about_screen.js

--- app/core/about/about_screen.js
+++ app/core/about/about_screen.js
@@ -4,23 +4,18 @@ $('#armrAboutBtn').click(function() {
     const html_content = 
 `<div style="text-align: center; padding-left: 20px; padding-right: 20px; color: rgb(200, 200, 200)">
 <div style="font-size: 20px"><b>Hey!</b></div>
-Thanks for checking out my project AtticRadar!
-This app was made by an independent developer - a (recently graduated) high school student.
+Thanks for checking out my project modRadar!
 
-You can find AtticRadar on Twitter: <a href="https://twitter.com/AtticRadar" style="color: #53a2e0;">@AtticRadar</a>
-Be sure to check there for updates and general info about the project.
+modRadar is a weather radar app designed to help people track storms, monitor severe weather, 
+and stay informed about changing conditions in real time. Whether you're watching approaching rain, 
+snow, or severe storms, modRadar helps you stay prepared and aware.
 
-AtticRadar is free to use and doesn't have any ads. \
-I hope that you enjoy the app, and feel free to shoot me a message on Twitter if you have a question! \
-My email is also open: <a href="mailto:steepatticstairs@gmail.com" style="color: #53a2e0;">steepatticstairs@gmail.com</a> \
-You can also message me on Discord; my username is <code class="about_box_code">steepatticstairs</code>.
+This app was made by an independent developer — a (recently graduated) high school student.
 
-Are you looking for AtticRadar's source code?
-It's on GitHub! Check it out:
-<a href="https://github.com/SteepAtticStairs/AtticRadar" style="color: #53a2e0;">https://github.com/SteepAtticStairs/AtticRadar</a>
-I’d appreciate giving credit if you do find the code helpful!
+modRadar is free to use and doesn't have any ads. 
+I hope that you enjoy the app! 
 
-Copyright © 2025 SteepAtticStairs. All rights reserved.
+Copyright © 2025. All rights reserved.
 </div>
 </div>`
 
